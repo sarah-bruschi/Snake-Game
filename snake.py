daaf4e0d3acee2f62f0be539f67368ee19648ac2
sarah-bruschi <sarah.bruschi@gmail.com>
2022-01-27 09:24:26 -0500

    allow the snake to move up, down, left, and right with checks (cannot go back on itself)

--- snake.py
+++ snake.py
@@ -1,13 +1,17 @@
 from turtle import Turtle
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 class Snake:
     """ Creates starting snake"""
     def __init__(self) -> None:
         self.segments = []
         self.create_snake()
-        pass
+        self.head = self.segments[0]
 
     def create_snake(self):
         """Create starting snake """
@@ -25,3 +29,19 @@ class Snake:
             new_y = self.segments[seg_num-1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
         self.segments[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
