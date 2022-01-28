52af9fb1f74afaaa55dfc3eb47e974768bcd2e00
sarah-bruschi <sarah.bruschi@gmail.com>
2022-01-28 09:13:20 -0500

    Create a Scoreboard and Keep Score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from food import Food
+from scoreboard import Scoreboard
 from snake import Snake
 from food import Food
 import time
@@ -12,6 +13,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up,"Up")
@@ -28,6 +30,7 @@ while game_is_on:
     #Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        scoreboard.increase_score()
 
 
 screen.exitonclick()
