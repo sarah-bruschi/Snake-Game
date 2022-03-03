9a6c47a556d71705441d8888ac66e43bd0a3f55e
sarah-bruschi <sarah.bruschi@gmail.com>
2022-03-03 08:30:35 -0500

    added highscore functionality

--- snake.py
+++ snake.py
@@ -26,6 +26,12 @@ class Snake:
         new_segment.goto(position)
         self.segments.append(new_segment)
     
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
     
     def extend(self):
         # add a new segment to the snake
